756ffb3deafea34ebfd67a96bc1a52db22251828
alaniel.garcia <alaniel.garcia@gmail.com>
2022-11-03 13:53:10 -0600

    refactor: songsList now receive songs by parent

--- src/components/SongsList.jsx
+++ src/components/SongsList.jsx
@@ -1,29 +1,17 @@
-import { useEffect, useState } from 'react';
-import './SongsList.scss';
 import TrackCard from './TrackCard';
-import UploadSongs from './UploadSongs';
+import './SongsList.scss'
 
-export default function SongsList(props) {
-    const [songs, setSongs] = useState([]);
-
-    useEffect(() => {
-        console.log('songs: ' + songs.length, songs);
-    }, [songs]);
+export default function SongsList({songs}) {
 
     return (
         <>
             <div className='SongsList'>
-                <div className='SongsList__header'>
-                    <div className='SongsList__header--left'></div>
-                    <div className='SongsList__header--right'>
-                        <UploadSongs container={setSongs} content={songs}/>
-                    </div>
-                </div>
-                <div className='SongsList__songs-container'>
+                <div className='SongsList__container'>
                     {songs &&
                         songs.map((song, i) => {
                             return <TrackCard key={i} song={song} songsList={songs} />;
-                        })}
+                        })
+                    }
                 </div>
             </div>
         </>
