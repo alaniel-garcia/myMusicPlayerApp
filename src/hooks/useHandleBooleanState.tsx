ab1269b7fe21465355138fef2908ec5eecb1ce7b
alaniel.garcia <alaniel.garcia@gmail.com>
2022-12-01 21:13:20 -0600

    minimum to production

--- src/hooks/useHandleBooleanState.tsx
+++
@@ -1,8 +0,0 @@
-
-//Handle each state that when it changes, it does it to the opposite boolean
-
-export default function useHandleBooleanState(setStateFunction : Function){
-    setStateFunction((prevState : boolean) => {
-        return !prevState
-    })
-}
