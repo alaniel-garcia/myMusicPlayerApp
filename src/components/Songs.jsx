42ae30189a36f505dfa0ef0ef8d625fa4b2e300b
alaniel.garcia <alaniel.garcia@gmail.com>
2023-02-21 22:21:30 -0600

    feat: add visual loader when downloading default songs to client

--- src/components/Songs.jsx
+++ src/components/Songs.jsx
@@ -5,7 +5,7 @@ import AddWhenNoSongs from './AddWhenNoSongs';
 import useLibraryContext from '@hooks/useLibraryContext';
 import * as IDBSongs from '../services/IDB';
 import handleAudio from '../services/handleAudio';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import defaultSongs from '../utils/defaultMusic';
 import handleFilesUpload from '../services/handleFilesUpload';
 import useIDBContext from '../hooks/useIDBContext';
@@ -14,41 +14,51 @@ export default function Songs({className}){
 
     const {library, updateLibrary} = useLibraryContext();
     const {IDB} = useIDBContext();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(()=>{
+        if(library.length > 0 && isLoading){
+            setIsLoading(false)
+        }
+    },[library]);
 
     useEffect(()=>{
         if(IDB){
             if(library.length === 0 ){
                 async function handleIDBData(){
-                    const IDBData = await IDBSongs.readDataFromUserMusic()
+                    try {
+                        const IDBData = await IDBSongs.readDataFromUserMusic()
+                        if(IDBData && IDBData.length > 0){
+                            const result = await Promise.all(IDBData.map( async (song) => {
+                                return await handleAudio(song)
+                            }))
 
-                    if(IDBData && IDBData.length > 0){
-                        const result = await Promise.all(IDBData.map( async (song) => {
-                            return await handleAudio(song)
-                        }))
+                            updateLibrary(result)
+                        }
+                        else {
+                            setIsLoading(true)
+                            const filtered = await Promise.all(defaultSongs.map( async (songURL) => {
+                                // return await new Promise((res, rej)=>{
+                                // const xml = new XMLHttpRequest();
+                                // xml.open('get', songURL)
+                                // xml.responseType = 'blob'
+                                // xml.onload = async (e) => {
+                                //     res(xml.response)
+                                // }
+                                // xml.send()
+                                // })
 
-                        updateLibrary(result)
+                                return fetch(songURL)
+                                    .then(res => res.blob())
+                                    .then(blob => blob)
+                            }))
+                            const result = await handleFilesUpload(filtered, true);
+                            updateLibrary(result)
+                        }
+                        return
+                    } catch (error) {
+                        console.log('Error loading default songs',error)
                     }
-                    else {
-                        const filtered = await Promise.all(defaultSongs.map( async (songURL) => {
-                            // return await new Promise((res, rej)=>{
-                            // const xml = new XMLHttpRequest();
-                            // xml.open('get', songURL)
-                            // xml.responseType = 'blob'
-                            // xml.onload = async (e) => {
-                            //     res(xml.response)
-                            // }
-                            // xml.send()
-                            // })
-
-                            return fetch(songURL)
-                                .then(res => res.blob())
-                                .then(blob => blob)
-                        }))
-                        const result = await handleFilesUpload(filtered, true);
-
-                        updateLibrary(result)
-                    }
-                    return
                 }
 
                 handleIDBData()
@@ -61,13 +71,27 @@ export default function Songs({className}){
             return <SongsList songs={library} cardType='default'/>
         }
         else{
-            return(
-                <div className='Songs__no-songs'>
-                    <AddWhenNoSongs>
-                        <UploadSongs anySong={true}/>
-                    </AddWhenNoSongs>
-                </div>
-            )
+            if(isLoading){
+                return(
+                    <div className='Songs__loading'>
+                        <div className='Songs__loading-info'>
+                            <div className='Songs__loading-loader'></div>
+                            <div className='Songs__loading-message'>
+                                <h2>Loading Default Songs</h2>
+                            </div>
+                        </div>
+                    </div>
+                )
+            }
+            else {
+                return(
+                    <div className='Songs__no-songs'>
+                        <AddWhenNoSongs>
+                            <UploadSongs anySong={true}/>
+                        </AddWhenNoSongs>
+                    </div>
+                )
+            }
         }
     }
 
