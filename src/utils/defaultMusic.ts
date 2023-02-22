dc6699b3817054e317b80f09b7c1b527cb5bea19
alaniel.garcia <alaniel.garcia@gmail.com>
2023-02-21 23:08:05 -0600

    chore: change some default songs

--- src/utils/defaultMusic.ts
+++ src/utils/defaultMusic.ts
@@ -1,11 +1,12 @@
 import savages from '@utils/music/default/Savages-Marina-And-The-Diamonds.mp3';
 import ifICantHaveYou from '@utils/music/default/Shawn-Mendes-If-I-Can-t-Have-You.mp3';
-import inMyBlood from '@utils/music/default/Shawn-Mendes-In-My-Blood.mp3';
+import wonder from '@utils/music/default/Shawn-Mendes-Wonder.mp3';
 import happier from '@utils/music/default/Marshmello-Happier.mp3';
 import honestly from '@utils/music/default/Eric-Nam-Honestly.mp3';
 import anxiety from '@utils/music/default/Julia-Michaels-Anxiety.mp3';
 import rescueMe from '@utils/music/default/OneRepublic-Rescue-Me.mp3';
 import peopleWatching from '@utils/music/default/People-Watching-Conan-Gray.mp3';
 import hunger from '@utils/music/default/Florence-The-Machine-Hunger.mp3';
+import secret from '@utils/music/default/Joshua-Bassett-Secret.mp3';
 
-export default [savages, ifICantHaveYou, inMyBlood, happier, honestly, anxiety, peopleWatching, hunger, rescueMe];
+export default [savages, ifICantHaveYou, wonder, happier, honestly, anxiety, peopleWatching, hunger, secret, rescueMe];
